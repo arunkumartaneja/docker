f0ab52307cd9b45ad024dcc2ee10e94f69d952f0
Arun Taneja <taneja.arun1@gmail.com>
2022-07-07 16:01:14 +0530

    react with docker and Nginx example

---
+++ nginx-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Deploy react app using docker and Nginx running @ 8081 !!!!</h1>
+    </div>
+  );
+}
+
+export default App;
